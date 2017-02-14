528d9da279ab3dd85f086e17ef7d63e23d31be7e
ctheilman92 <ctheilman92@gmail.com>
2017-02-13 21:29:53 -0600

    progress

--- XF_FetchJson/XF_FetchJson/Models/User.cs
+++ XF_FetchJson/XF_FetchJson/Models/User.cs
@@ -3,20 +3,70 @@ using System.Collections.Generic;
 using System.Text;
 using Newtonsoft.Json;
 
-namespace XF_FetchJson.Models
-{
-    class User {
-        private int id { get; set; }
+namespace XF_FetchJson.Models {
+	public class Geo {
 
-        private string name { get; set; }
+		[JsonProperty("lat")]
+		public string lat { get; set; }
 
-        private string username { get; set; }
-        public List<string> ulist { get; set; }
+		[JsonProperty("lng")]
+		public string lng { get; set; }
+	}
 
-        public User() {
-        }
+	public class Address {
 
+		[JsonProperty("street")]
+		public string street { get; set; }
 
+		[JsonProperty("suite")]
+		public string suite { get; set; }
 
-    }
+		[JsonProperty("city")]
+		public string city { get; set; }
+
+		[JsonProperty("zipcode")]
+		public string zipcode { get; set; }
+
+		[JsonProperty("geo")]
+		public Geo geo { get; set; }
+	}
+
+	public class Company {
+
+		[JsonProperty("name")]
+		public string name { get; set; }
+
+		[JsonProperty("catchPhrase")]
+		public string catchPhrase { get; set; }
+
+		[JsonProperty("bs")]
+		public string bs { get; set; }
+	}
+
+	public class User {
+
+		[JsonProperty("id")]
+		public int id { get; set; }
+
+		[JsonProperty("name")]
+		public string name { get; set; }
+
+		[JsonProperty("username")]
+		public string username { get; set; }
+
+		[JsonProperty("email")]
+		public string email { get; set; }
+
+		[JsonProperty("address")]
+		public Address address { get; set; }
+
+		[JsonProperty("phone")]
+		public string phone { get; set; }
+
+		[JsonProperty("website")]
+		public string website { get; set; }
+
+		[JsonProperty("company")]
+		public Company company { get; set; }
+	}
 }
--- XF_FetchJson/XF_FetchJson/Pages/fetchPage.cs
+++ XF_FetchJson/XF_FetchJson/Pages/fetchPage.cs
@@ -9,29 +9,45 @@ using System.Threading.Tasks;
 using XF_FetchJson.Models;
 using XLabs;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Xamarin.Forms;
 using System.Threading;
+using System.Diagnostics.Contracts;
 
 namespace XF_FetchJson.Pages
 {
 	public class fetchPage : ContentPage
 	{
+
+		#region variables
 		private static ListView fetchlv;
 		private static Button fetchBtn;
 		private static Entry getID;
-
-
+		private static List<User> ulist;
+		private static DataTemplate dt;
+		private static int userid;
+		private static bool doesExist;
 		private static string fetchurl = string.Format("http://jsonplaceholder.typicode.com/users/");
 
-		
+		#endregion
+
 
 		public fetchPage ()
 		{
-			fetchlv = new ListView() { RowHeight = 40 };
-			fetchBtn = new Button() { Text = "Fetch Json" };
-			getID = new Entry() { Placeholder = "User id #" };
-			
 			double topPadding = Device.OnPlatform<double>(20, 0, 0);
+			ulist = new List<User>();
+			getID = new Entry() {
+				Placeholder = "User id #",
+				Keyboard = Keyboard.Numeric,
+				TextColor = Color.Green
+			};
+			fetchBtn = new Button() { Text = "Fetch Json" };
+
+
+				
+			fetchlv = new ListView() {
+				RowHeight = 40,
+			};
 
 			var stack = new StackLayout() {
 				VerticalOptions = LayoutOptions.FillAndExpand,
@@ -40,34 +56,55 @@ namespace XF_FetchJson.Pages
 			};
 			this.Padding = new Thickness(0, topPadding, 0, 0);
 			this.Content = stack;
-			getData("1");
+
+			getID.TextChanged += (sender, e) => {
+				userid = Int16.Parse(e.NewTextValue);
+			};
+
+			//fetchBtn.Clicked += async delegate { await getData(userid); };
+
+			fetchBtn.Clicked += async (sender, e) => { await getData(userid); };
+
 		}
 
-		
-		
 
+		public async Task getData(int id) {
 
-		async static Task<string> getData(string id) {
-
-			User user = new User();
-			
-
-			try {
-				var client = new HttpClient();
-
-				client.DefaultRequestHeaders.Add("Accept", "application/json");
-				var address = $"http://jsonplaceholder.typicode.com/{id}";
-
-				var res = await client.GetAsync(address);
-				var resjson = res.Content.ReadAsStringAsync().Result;
-
-				return resjson;
-
+			foreach (var i in ulist) {
+				Console.WriteLine(i.id);
+				doesExist = (i.id == id) ? true : false;
+				Console.WriteLine(doesExist);
 			}
-			catch (System.Exception ex) {
-				Console.WriteLine(ex.Message);
-				throw;
-				
+
+			if (!doesExist) {
+				HttpClient client = new HttpClient();
+
+				try {
+
+					client.DefaultRequestHeaders.Add("Accept", "application/json");
+					var res = await client.GetAsync(fetchurl + id.ToString());
+
+					res.EnsureSuccessStatusCode();
+					var resjson = res.Content.ReadAsStringAsync().Result;
+					User user = JsonConvert.DeserializeObject<User>(resjson);
+
+					//check if user exists
+
+					ulist.Add(user);
+
+					dt = new DataTemplate(typeof(TextCell));
+					dt.SetBinding(TextCell.TextProperty, "email");
+					fetchlv.ItemsSource = ulist;
+					fetchlv.ItemTemplate = dt;
+					getID.Text = null;
+
+
+				} catch (Exception ex) {
+					Console.WriteLine(ex.Message);
+				}
+			} 
+			else {
+				await DisplayAlert(" (>.<) User Exists (>.<)", "Sorry, but it seems this user cannot be added more than once...", "OK");
 			}
 		}
 	}
